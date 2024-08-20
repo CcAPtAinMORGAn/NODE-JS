b5dd633bc4a51cb81283f718f51f90e46e01beef
CAPTAIN MORGAN <136248466+CcAPtAinMORGAn@users.noreply.github.com>
2024-08-20 09:50:06 +0000

    Add about.html

--- Node-Routing/index.js
+++ Node-Routing/index.js
@@ -11,3 +11,30 @@ const home=fs.readFileSync('home.html');
 const about=fs.readFileSync('about.html');
 const contact=fs.readFileSync('contact.html');
 const services=fs.readFileSync('services.html');
+
+const server=http.createServer((req,res)=>{
+    console.log(req.url)
+
+    res.statusCode=200;
+    res.setHeader('content-Type','text/html');
+    if(req.url=='/' || req.url=='/home'){
+        res.end(home);
+    }
+    else if(req.url=='/' || req.url=='/about'){
+        res.end(about);
+    }
+    else if(req.url=='/' || req.url=='/contact'){
+        res.end(contact);
+    }
+    else if(req.url=='/' || req.url=='/services'){
+        res.end(services);
+    }
+    else{
+        res.statusCode=404;
+        res.end("<h1>404 not Found</h1>")
+    }
+})
+
+server.listen(port,hostname,()=>{
+    console.log(`server running at http://${hostname}:${port}/`);
+});
